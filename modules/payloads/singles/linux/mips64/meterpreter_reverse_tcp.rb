22d0810065e25b8bbe7a4d520650ee127e3addc3
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2017-12-27 15:36:54 +0100

    New upstream version 4.16.26

--- modules/payloads/singles/linux/mips64/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/linux/mips64/meterpreter_reverse_tcp.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_mips64_linux'
 
 module MetasploitModule
 
-  CachedSize = 1081096
+  CachedSize = 1384304
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
--- modules/payloads/singles/linux/mipsle/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/linux/mipsle/meterpreter_reverse_tcp.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_mipsle_linux'
 
 module MetasploitModule
 
-  CachedSize = 1058584
+  CachedSize = 1281832
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
