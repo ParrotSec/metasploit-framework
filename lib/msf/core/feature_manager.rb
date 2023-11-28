7addb221bf530e7ea97610d06a52f60957542e60
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2023-11-28 17:52:30 +0100

    New upstream version 6.3.44

--- lib/msf/core/feature_manager.rb
+++ lib/msf/core/feature_manager.rb
@@ -18,6 +18,8 @@ module Msf
     DATASTORE_FALLBACKS = 'datastore_fallbacks'
     FULLY_INTERACTIVE_SHELLS = 'fully_interactive_shells'
     MANAGER_COMMANDS = 'manager_commands'
+    METASPLOIT_PAYLOAD_WARNINGS = 'metasploit_payload_warnings'
+    DEFER_MODULE_LOADS = 'defer_module_loads'
     DEFAULTS = [
       {
         name: WRAPPED_TABLES,
@@ -39,6 +41,18 @@ module Msf
         description: 'When enabled you can consistently set username across modules, instead of setting SMBUser/FTPUser/BIND_DN/etc',
         requires_restart: true,
         default_value: true
+      }.freeze,
+      {
+        name: METASPLOIT_PAYLOAD_WARNINGS,
+        description: 'When enabled Metasploit will output warnings about missing Metasploit payloads, for instance if they were removed by antivirus etc',
+        requires_restart: true,
+        default_value: false
+      }.freeze,
+      {
+        name: DEFER_MODULE_LOADS,
+        description: 'When enabled will not eagerly load all modules',
+        requires_restart: true,
+        default_value: false
       }.freeze
     ].freeze
 
